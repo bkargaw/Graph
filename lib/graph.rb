5959d4598c940c94432fd638ca5e800365e98c37
Bruk Argaw <bkargaw@gmail.com>
2017-07-11 15:51:23 -0700

    finished creating the graph

--- lib/graph.rb
+++ lib/graph.rb
@@ -1,15 +1,44 @@
 class Vertex
-  def initialize(value)
 
+  attr_reader :value, :in_edges, :out_edges
+  def initialize(value)
+    @value = value
+    @in_edges = []
+    @out_edges = []
+  end
+
+  def add(edge, direction)
+    case direction
+    when 'out'
+      @out_edges.push(edge)
+    when 'in'
+      @in_edges.push(edge)
+    end
+  end
+  def destroy(edge, direction)
+    case direction
+    when 'out'
+      @out_edges.delete_if { |e| e == edge }
+    when 'in'
+      @in_edges.delete_if { |e| e == edge }
+    end
   end
 end
 
 class Edge
+  attr_reader :from_vertex, :to_vertex, :cost
   def initialize(from_vertex, to_vertex, cost = 1)
-
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+    @from_vertex.add(self, 'out')
+    @to_vertex.add(self, 'in')
   end
 
   def destroy!
-
+    @from_vertex.destroy(self, 'out')
+    @to_vertex.destroy(self, 'in')
+    @from_vertex = nil
+    @to_vertex = nil
   end
 end
