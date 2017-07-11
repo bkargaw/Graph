0b259adcf7d99aba7f2c3115f468a1987b3d676d
Bruk Argaw <bkargaw@gmail.com>
2017-07-11 16:46:01 -0700

    finished khans topological sort agr

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -1,7 +1,23 @@
 require_relative 'graph'
+require 'byebug'
 
 # Implementing topological sort using both Khan's and Tarian's algorithms
-
+ # Khan's topological sorting algorithms
 def topological_sort(vertices)
+  next_vertex = Queue.new
+  result = []
+  vertices.each do |vertex|
+    next_vertex.push(vertex) if vertex.in_edges.length < 1
+  end
 
+  while !next_vertex.empty?
+    vertex = next_vertex.pop
+    vertex.out_edges.each do |edge|
+      new_vertex = edge.to_vertex
+      next_vertex.push(new_vertex) if new_vertex.in_edges.length <= 1
+    end
+    vertex.out_edges.each{|edge| edge.destroy!}
+    result.push(vertex)
+  end
+  result
 end
